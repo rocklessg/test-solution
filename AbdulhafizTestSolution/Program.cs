f7bec736d5ac33ad29f455f22431a7911287245e
rocklessg <abdulhafiz.suleiman@decagon.dev>
2024-04-27 08:24:44 +0100

    Solution completed

--- AbdulhafizTestSolution/Program.cs
+++ AbdulhafizTestSolution/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AbdulhafizTestSolution;
+
 Console.WriteLine("Hello, World!");
+
+var testCase1 = TestAnswer.RobberyAlertSum([1, 2, 3, 1]);
+Console.WriteLine(testCase1);
+var testCase2 = TestAnswer.RobberyAlertSum([2, 7, 9, 3, 1]);
+Console.WriteLine(testCase2);
+
+var testCase3 = TestAnswer.RobberyAlertSum([1, 2, 3, 1, 6]);
+Console.WriteLine(testCase3);
+var testCase4 = TestAnswer.RobberyAlertSum([2, 7, 9, 3, 1, 2, 3]);
+Console.WriteLine(testCase4);
---
+++ AbdulhafizTestSolution/TestAnswer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbdulhafizTestSolution
+{
+    public class TestAnswer
+    {
+        public static int RobberyAlertSum(int[] money)
+        {
+            var totalMoney = 0;
+            for (int i = 0; i < money.Length; i++)
+            {
+                var indexIncrement = i + 1;
+                //indexIncrement++;
+                // Console.WriteLine(indexIncrement);
+                if (indexIncrement % 2 != 0)
+                {
+                    totalMoney += money[i];
+                }
+            }
+            return totalMoney;
+        }
+
+    }
+}
